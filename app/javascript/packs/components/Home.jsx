3731554800d6db84e131e1e845e591b2d6376531
Alexander <alexrqs@users.noreply.github.com>
2017-03-23 01:22:08 -0500

    Add/static picture (#1)
    * add profile pic static
    
    * add profile pic static

--- app/javascript/packs/components/Home.jsx
+++ app/javascript/packs/components/Home.jsx
@@ -7,6 +7,19 @@ class Home extends Component {
 
     this.textAreaAdjust = this.textAreaAdjust.bind(this)
     this.simulateUpload = this.simulateUpload.bind(this)
+    this.readURL = this.readURL.bind(this)
+  }
+
+  readURL(event) {
+    if (event.currentTarget.files && event.currentTarget.files[0]) {
+      const reader = new FileReader()
+
+      reader.onload = (e) => {
+        this.profilePicture.setAttribute('src', e.target.result)
+      }
+
+      reader.readAsDataURL(event.currentTarget.files[0])
+    }
   }
 
   simulateUpload() {
@@ -22,14 +35,24 @@ class Home extends Component {
     return (
       <div>
         <form className="profile__form">
-          <button onClick={this.simulateUpload} className="profile__simulator">
+          <label htmlFor="file">
             <img
-              src="http://lorempixel.com/200/200" alt="name"
+              src="http://www.sheffield.com/wp-content/uploads/2013/06/placeholder.png"
+              alt="name"
+              height={150}
+              width={150}
               className="profile__picture"
+              ref={node => { this.profilePicture = node }}
             />
-          </button>
+            <input
+              type="file"
+              id="file"
+              ref={node => { this.file = node }}
+              hidden
+              onChange={this.readURL}
+            />
+          </label>
 
-          <input type="file" ref={node => { this.file = node }} hidden />
           <br />
 
           <input
@@ -43,7 +66,7 @@ class Home extends Component {
 
           <textarea
             ref={node => { this.description = node }}
-            placeholder="Why do you do sports in 140 chars."
+            placeholder="Why do you do sports."
             className="profile__description"
             onKeyUp={this.textAreaAdjust}
           />
