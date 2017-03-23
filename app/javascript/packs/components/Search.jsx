66325f5f5c55c4e2d215fef71154563986681396
Alexander <alexrqs@users.noreply.github.com>
2017-03-23 15:37:08 -0500

    add geolocation detector on the front (#3)

--- app/javascript/packs/components/Search.jsx
+++ app/javascript/packs/components/Search.jsx
@@ -24,6 +24,50 @@ class Home extends Component {
   constructor() {
     super()
 
+    this.state = {
+      searchState: 'Check the sports you practice and hit search',
+    }
+
+    this.submitSearch = this.submitSearch.bind(this)
+  }
+
+  submitSearch(event) {
+    event.preventDefault()
+
+    function position(arg) {
+      this.setState({
+        searchState: 'We are loonking for your Sportsmate',
+      })
+    }
+
+    function error(err) {
+      let message
+
+      switch (err.code) {
+        case err.PERMISSION_DENIED:
+          message = 'User denied the request for Geolocation.'
+          break
+        case err.POSITION_UNAVAILABLE:
+          message = 'Location information is unavailable.'
+          break
+        case err.TIMEOUT:
+          message = 'The request to get user location timed out.'
+          break
+        case err.UNKNOWN_ERROR:
+          message = 'An unknown error occurred.'
+          break
+        default:
+      }
+
+      this.setState({ searchState: message })
+    }
+
+    if (navigator.geolocation) {
+      console.log(navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(position.bind(this), error.bind(this))
+    } else {
+      // no geo available
+    }
   }
 
   render() {
@@ -33,7 +77,9 @@ class Home extends Component {
           within
         </header>
 
-        <form className="search__form">
+        <h3>{this.state.searchState}</h3>
+
+        <form className="search__form" onSubmit={this.submitSearch}>
 
           <div className="search__checks">
             {SPORTS.map(sport => <CheckBox name={sport} key={sport} />)}
