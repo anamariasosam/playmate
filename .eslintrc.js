3d1e6cd5049f38018d9a00de525451fcfb61a755
Alex <alexlopezarroyave@gmail.com>
2017-03-23 00:08:39 -0500

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,15 @@ module.exports = {
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+
+    env: {
+        browser: true,
+    },
+
+    rules: {
+        semi: [1, 'never'],
+        'arrow-parens': [1, 'as-needed'],
+        'react/prefer-stateless-function': ['warn'],
+    },
 };
