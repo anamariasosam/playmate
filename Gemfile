504778fdd8457a654614347bb32af40ee97090ab
Ana Maria Sosa <anamariasosam@gmail.com>
2017-03-23 23:36:23 -0500

    Add picture to users

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'active_model_serializers'
+gem 'carrierwave', '~> 1.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :sports, through: :workouts
 
   validates_presence_of :name, :description
+
+  mount_uploader :picture, PictureUploader
 end
