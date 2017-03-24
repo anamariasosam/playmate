e2f3a05e9df7f67ff6f35649015ec5d2696bb7e8
Alex <alexlopezarroyave@gmail.com>
2017-03-24 01:03:25 -0500

    attempt to connect with workout

--- app/javascript/packs/constants/index.js
+++ app/javascript/packs/constants/index.js
@@ -1,1 +1,10 @@
 export const LOCAL_KEY = 'playmate'
+export const SPORTS = [
+  'Walk',
+  'Run',
+  'Cycle',
+  'Tennis',
+  'Squash',
+  'Shoot',
+  'PingPong',
+]
--- app/javascript/packs/scenes/Search.jsx
+++ app/javascript/packs/scenes/Search.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'
+import superagent from 'superagent'
 
-const SPORTS = [
-  'Walk',
-  'Run',
-  'Cycle',
-  'Tennis',
-  'Squash',
-  'Shoot',
-  'PingPong',
-]
+import { LOCAL_KEY, SPORTS } from '../constants'
 
 const CheckBox = props => {
   return (
-    <label className="search__check">
-      <input type="checkbox" hidden />
+    <label className="search__check" htmlFor={`check_${props.name}`}>
+      <input
+        type="checkbox"
+        hidden
+        onChange={props.onChange}
+        value={props.name}
+        id={`check_${props.name}`}
+      />
       <span className="search__checktext">{props.name}</span>
     </label>
   )
@@ -29,44 +28,70 @@ class Search extends Component {
     }
 
     this.submitSearch = this.submitSearch.bind(this)
+    this.handleCheckbox = this.handleCheckbox.bind(this)
+    this.errorOnGeo = this.errorOnGeo.bind(this)
   }
 
   submitSearch(event) {
     event.preventDefault()
 
-    function position(arg) {
+
+    function position() {
       this.setState({
         searchState: 'We are loonking for your Sportsmate',
       })
     }
 
-    function error(err) {
-      let message
-
-      switch (err.code) {
-        case err.PERMISSION_DENIED:
-          message = 'User denied the request for Geolocation.'
-          break
-        case err.POSITION_UNAVAILABLE:
-          message = 'Location information is unavailable.'
-          break
-        case err.TIMEOUT:
-          message = 'The request to get user location timed out.'
-          break
-        case err.UNKNOWN_ERROR:
-          message = 'An unknown error occurred.'
-          break
-        default:
-      }
-
-      this.setState({ searchState: message })
-    }
-
     if (navigator.geolocation) {
       console.log(navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(position.bind(this), error.bind(this))
+      navigator.geolocation.getCurrentPosition(position.bind(this), this.errorOnGeo)
     } else {
       // no geo available
+    }
+  }
+
+  errorOnGeo(err) {
+
+    let message
+
+    switch (err.code) {
+      case err.PERMISSION_DENIED:
+        message = 'User denied the request for Geolocation.'
+        break
+      case err.POSITION_UNAVAILABLE:
+        message = 'Location information is unavailable.'
+        break
+      case err.TIMEOUT:
+        message = 'The request to get user location timed out.'
+        break
+      case err.UNKNOWN_ERROR:
+        message = 'An unknown error occurred.'
+        break
+      default:
+    }
+
+    this.setState({ searchState: message })
+  }
+
+  handleCheckbox(event) {
+    const { userId = null } = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {}
+
+    if (userId && event.currentTarget.checked) {
+      superagent
+        .post('/api/workouts')
+        .query({
+          user_id: Number(userId) - 1,
+          sport_id: SPORTS.indexOf(event.currentTarget.value) + 1,
+        })
+        .end((err, res) => {
+          if (err) { return }
+
+          console.log(res.body);
+        })
+    }
+
+    if (userId && !event.currentTarget.checked) {
+      // delete sport
     }
   }
 
@@ -74,7 +99,7 @@ class Search extends Component {
     return (
       <div className="search">
         <header>
-          within
+          <h2 className="search__title">Search</h2>
         </header>
 
         <h3>{this.state.searchState}</h3>
@@ -82,7 +107,9 @@ class Search extends Component {
         <form className="search__form" onSubmit={this.submitSearch}>
 
           <div className="search__checks">
-            {SPORTS.map(sport => <CheckBox name={sport} key={sport} />)}
+            {SPORTS.map(sport => (
+              <CheckBox name={sport} key={sport} onChange={this.handleCheckbox} />
+            ))}
           </div>
 
           <br />
