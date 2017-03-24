1769f1110d52420cbd5c8135f6b5e2f52eb83f1c
Ana Maria Sosa <anamariasosam@gmail.com>
2017-03-23 22:17:08 -0500

    Add workout model (many to many)

---
+++ app/controllers/api/workouts_controller.rb
@@ -0,0 +1,69 @@
+class Api::WorkoutsController < ApplicationController
+  before_action :set_workout, only: [:show, :edit, :update, :destroy]
+
+  # GET /workouts
+  # GET /workouts.json
+  def index
+    @workouts = Workout.all
+    render json: @workouts
+  end
+
+  # GET /workouts/1
+  # GET /workouts/1.json
+  def show
+    render json: @workout
+  end
+
+  # GET /workouts/new
+  def new
+    @workout = Workout.new
+  end
+
+  # GET /workouts/1/edit
+  def edit
+  end
+
+  # POST /workouts
+  # POST /workouts.json
+  def create
+    @workout = Workout.new(workout_params)
+    @workout.user_id = current_user.id
+    if @workout.save
+      render json: @workout, status: :created
+    else
+      render json: @workout.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /workouts/1
+  # PATCH/PUT /workouts/1.json
+  def update
+    respond_to do |format|
+      if @workout.update(workout_params)
+        format.json { render :show, status: :ok }
+      else
+        format.json { render json: @workout.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /workouts/1
+  # DELETE /workouts/1.json
+  def destroy
+    @workout.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_workout
+      @workout = Workout.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def workout_params
+      params.permit(:sport_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
+    resources :workouts
     resources :sports
     resources :users
   end
