116f9ccadc3b270f584865f4a88a6e886d418496
Ana Maria Sosa <anamariasosam@gmail.com>
2017-03-28 17:02:59 -0500

    Add welcome material ui style

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,10 +1,12 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
-
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import Start from './scenes/Start'
 import Search from './scenes/Search'
@@ -13,13 +15,15 @@ import Welcome from './scenes/Welcome'
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/start" component={Start} />
-          <Route path="/search" component={Search} />
-        </div>
-      </Router>
+      <MuiThemeProvider>
+        <Router>
+          <div>
+            <Route exact path="/" component={Welcome} />
+            <Route path="/start" component={Start} />
+            <Route path="/search" component={Search} />
+          </div>
+        </Router>
+      </MuiThemeProvider>
     )
   }
 }
--- app/javascript/packs/scenes/Welcome.jsx
+++ app/javascript/packs/scenes/Welcome.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton';
 
 import { LOCAL_KEY } from '../constants'
 
@@ -29,7 +30,15 @@ class Welcome extends Component {
         <h2 className="welcome__headline">Welcome to</h2>
         <h1 className="welcome__name">Playmate</h1>
         <p className="welcome__deck">Connecting people to practice the sports they love</p>
-        <Link className="welcome__start" to={this.state.goto}>Start</Link>
+
+        <RaisedButton
+          containerElement={<Link to={this.state.goto} />}
+          linkButton={true}
+          primary={true}
+          fullWidth={true}
+          label='Start'
+          className="welcome__start"
+        />
       </div>
     )
   }
